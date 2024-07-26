0a68087dd8dd9d557c37eb38959358613a9d630d
PlotYeast <lyddagiraldo@gmail.com>
2024-07-26 18:55:55 -0400

    Added Displaycode for the pattern needed to be implemented

--- CircusSpin/Assets/Scripts/TrickPatternCreator.cs
+++ CircusSpin/Assets/Scripts/TrickPatternCreator.cs
@@ -2,20 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrickPatternCreator : MonoBehaviour
 {
-    //List<string> patterns = new List<string>();
-    int difficultyScale = 2;
+    List<string> patterns = new List<string>();
+
+    int difficultyScale = 9;
     int numOfCorrectInputs = 0;
+    
     string pattern = "";
     string playerPattern = "";
-    
+
+    public GameObject upArrow;
+    public GameObject downArrow;
+    public GameObject leftArrow;
+    public GameObject rightArrow;
+
+    [SerializeField] Camera mainCamera = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateNewInputs();
+        DisplayInputs();
     }
 
     // Update is called once per frame
@@ -24,31 +34,59 @@ public class TrickPatternCreator : MonoBehaviour
         
     }
     
-    public void CreateNewInputs()
+    private void CreateNewInputs()
     {
         for (int i = 0; i < difficultyScale; i++)
         {
             switch (Random.Range(0, 4))
             {
                 case 0:
-                    // patterns.Add("upkey");
+                    patterns.Add("up");
                     pattern += "up";
                     break;
                 case 1:
-                    // patterns.Add("downkey");
+                    patterns.Add("down");
                     pattern += "down";
                     break;
                 case 2:
-                    //  patterns.Add("leftkey");
+                    patterns.Add("left");
                     pattern += "left";
                     break;
                 case 3:
                     pattern += "right";
-                   // patterns.Add("rightkey");
+                    patterns.Add("right");
+                    break;
+            } 
+        }
+    }
+
+    public void DisplayInputs()
+    {
+        Vector3 position = Vector3.zero;
+        float xposition = -2f;
+        position.y = 1.5f;
+        
+
+        foreach (string pattern in patterns)
+        {
+            position.x = xposition;
+            switch (pattern)
+            {
+                case "up":
+                    Instantiate(upArrow, position, Quaternion.identity, transform);
+                    break;
+                case "down":
+                    Instantiate(downArrow, position, Quaternion.identity, transform);
+                    break;
+                case "left":
+                    Instantiate(leftArrow, position, Quaternion.identity, transform);
+                    break;
+                case "right":
+                    Instantiate(rightArrow, position, Quaternion.identity, transform);
                     break;
             }
-                
-            
+
+            xposition += 0.5f;
         }
     }
 
@@ -58,6 +96,8 @@ public class TrickPatternCreator : MonoBehaviour
         {
             playerPattern += playerInput;
         }
+        
+
         numOfCorrectInputs = Regex.Matches(playerPattern, pattern).Count;
 
         pattern = "";
