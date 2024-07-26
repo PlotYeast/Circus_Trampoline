663098466f7754f8e5ca5a4377ad58bd0c0b8ef2
Hydrylith <86570256+Hydrylith@users.noreply.github.com>
2024-07-26 16:08:36 -0700

    Changed Bounce Calculations

--- CircusSpin/Assets/Scripts/PlayerBounce.cs
+++ CircusSpin/Assets/Scripts/PlayerBounce.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class PlayerBounce : MonoBehaviour
 {
-    float airTime = 0;
-    float spinTime;
     int score = 0;
-    [SerializeField] float jumpForce;
-    [SerializeField] float timeMultiplier = 1.5f;
+    [SerializeField] float jumpForce = 10.0f;
+    [SerializeField] float jumpModifier = 0.5f;
+    [SerializeField] float jumpForceMax = 10.0f;
     [SerializeField] AudioSource Bounce1;
     [SerializeField] AudioSource Bounce2;
     [SerializeField] AudioSource Bounce3;
     SpinManager spinner;
-    TrickPatternCreator patternCreator;
+    [SerializeField] TrickPatternCreator patternCreator;
     Rigidbody2D rB;
     // Start is called before the first frame update
     void Start()
@@ -25,7 +24,7 @@ public class PlayerBounce : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        airTime += Time.deltaTime;
+        
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -44,24 +43,28 @@ public class PlayerBounce : MonoBehaviour
             default:
                 break;
         }
-        //int inputSequences = patternCreator.CompareInputs(spinner.GetPlayerInputs());
-        //spinner.AddScore(inputSequences);
+        int inputSequences = patternCreator.CompareInputs(spinner.GetPlayerInputs());
+        print(inputSequences);
+        spinner.AddScore(inputSequences);
+        if (inputSequences == 0) { jumpForce -= jumpModifier; }
+        else
+        {
+            jumpForce += jumpModifier;
+            if (jumpForce > jumpForceMax) jumpForce = jumpForceMax;
+        }
         foreach (string input in spinner.GetPlayerInputs()) 
         {
             print(input.ToString());
         }
         spinner.ClearInputs();
-        float timeSpun = spinTime / airTime * timeMultiplier;
-        rB.velocity = new Vector2(0, timeSpun*jumpForce);
-        if (spinTime == 0) 
-        {
-            spinner.loseGame();
-        }
-        airTime = 0;
-        spinTime = 0;
+        
+        rB.velocity = new Vector2(0, jumpForce);
+        if (jumpForce <= 0) { spinner.loseGame();}
     }
-    public void AddSpinTime(float extraTime) 
+    public void AddSpinTime(float spinTime) 
     {
-        spinTime += extraTime;
+        //I wanted to delete this function because we're no longer using spinTime
+        //But that would break code in SpinManager, which someone else is working on.
+        //So this is an empty function to prevent things from breaking.
     }
 }
