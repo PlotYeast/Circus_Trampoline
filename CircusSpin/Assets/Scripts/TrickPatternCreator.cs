7737495f2b896bf9e272b7628dfc237ce98002bc
PlotYeast <lyddagiraldo@gmail.com>
2024-07-29 18:33:51 -0400

    Input Sprites now work correctly

--- CircusSpin/Assets/Scripts/TrickPatternCreator.cs
+++ CircusSpin/Assets/Scripts/TrickPatternCreator.cs
@@ -3,29 +3,47 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class TrickPatternCreator : MonoBehaviour
 {
     List<string> patterns = new List<string>();
 
+    List<GameObject> arrowObjects = new List<GameObject>();
+
     int difficultyScale = 2;
     int numOfCorrectInputs = 0;
+    int numOfBounce = 0;
     
     string pattern = "";
     string playerPattern = "";
 
-    public GameObject upArrow;
-    public GameObject downArrow;
-    public GameObject leftArrow;
-    public GameObject rightArrow;
+    public Sprite upArrow;
+    public Sprite downArrow;
+    public Sprite leftArrow;
+    public Sprite rightArrow;
 
-    [SerializeField] Camera mainCamera = null;
+    public GameObject ArrowDefault;
 
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 position = Vector3.zero;
+        float xposition = -2f;
+        position.y = 1.5f;
+
+
+        for (int i = 0; i<9; i++)
+        {
+            position.x = xposition;
+            arrowObjects.Add(Instantiate(ArrowDefault, position, Quaternion.identity, transform));
+            Debug.Log("created and added");
+            xposition += 0.5f;
+        }
+            
+
         CreateNewInputs();
-        DisplayInputs();
+        //DisplayInputs();
     }
 
     
@@ -33,23 +51,36 @@ public class TrickPatternCreator : MonoBehaviour
     {
         for (int i = 0; i < difficultyScale; i++)
         {
-            switch (Random.Range(0, 4))
+            switch (UnityEngine.Random.Range(0, 4))
             {
                 case 0:
                     patterns.Add("up");
                     pattern += "up";
+
+                    arrowObjects[i].GetComponent<SpriteRenderer>().sprite = upArrow;
+
                     break;
                 case 1:
                     patterns.Add("down");
                     pattern += "down";
+
+                    arrowObjects[i].GetComponent<SpriteRenderer>().sprite = downArrow;
+
                     break;
                 case 2:
                     patterns.Add("left");
                     pattern += "left";
+
+                    arrowObjects[i].GetComponent<SpriteRenderer>().sprite = leftArrow;
+
                     break;
+
                 case 3:
                     pattern += "right";
                     patterns.Add("right");
+
+                    arrowObjects[i].GetComponent<SpriteRenderer>().sprite = rightArrow;
+
                     break;
             } 
         }
@@ -97,6 +128,10 @@ public class TrickPatternCreator : MonoBehaviour
 
         pattern = "";
         playerPattern = "";
+
+        numOfBounce += 1;
+
+        difficultyScale = Math.Clamp((numOfBounce / 4), 2, 9);
 
         return numOfCorrectInputs;
     }
